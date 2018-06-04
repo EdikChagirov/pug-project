b879a7b70f9f8c83377430a867691e77c6b86eab
Edward Chagirov <echagirov@simtechdev.com>
2018-06-05 00:16:30 +0400

    Finish setting pug template

---
+++ source/pages/blog/blog.js
@@ -0,0 +1,7 @@
+import createMenu from '../../components/menu/menu';
+
+var menu = createMenu(['Blog item 1', ['Blog item 2']], 'menu');
+
+document.body.appendChild(menu);
+
+console.log('in blog.js');
